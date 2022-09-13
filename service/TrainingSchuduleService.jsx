1f148c242742ab35e58b9ff0d7652958a14a25f2
Alpha112355 <113292383+Alpha112355@users.noreply.github.com>
2022-09-13 10:45:16 +0530

    Update TrainingSchuduleService.jsx

--- service/TrainingSchuduleService.jsx
+++ service/TrainingSchuduleService.jsx
@@ -2,9 +2,16 @@ import axios from "axios";
 
 const TRAININGSCHUDULE_REST_API_URL = 'http://localhost:8080/sms/TrainingSchudle';
 
+//const COURSE_REST_API_URL  ='http://localhost:8080/sms/Course';
+
+
 class TrainingSchuduleService{
     getAllTrainingSchudule(){
         return axios.get(TRAININGSCHUDULE_REST_API_URL );
+    }
+
+    createTrainingSchudule(trainingSchudule){
+        return axios.post(TRAININGSCHUDULE_REST_API_URL , trainingSchudule)
     }
 }
 
