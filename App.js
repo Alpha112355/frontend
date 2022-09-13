ab518fabd6ddd931b0613342d5bbbab518e76896
Alpha112355 <113292383+Alpha112355@users.noreply.github.com>
2022-09-13 10:48:48 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Menu from './components/Menu';
+import UserMenu from './components/UserMenu';
 import GetCourseComponent from './coursecomponent/GetCourseComponent';
 import GetPaymentComponent from './paymentcomponent/GetPaymentComponent';
 import GetProgressComponent from './progresscomponent/GetProgressComponent';
@@ -12,6 +12,12 @@ import GetProgressComponent from './progresscomponent/GetProgressComponent';
 import GetStudentComponent from './studentcomponent/GetStudentComponent';
 import GetTrainerComponent from './trainercomponent/GetTrainerComponent';
 import TrainingSchuduleComponent from './trainingschudulecomponent/TrainingSchuduleComponent';
+import AddStudentComponent from './studentcomponent/AddStudentComponent';
+import AddProgressComponent from './progresscomponent/AddProgressComponent';
+import AddCourseComponent from './coursecomponent/AddCourseComponent';
+import AddPaymentComponent from './paymentcomponent/AddPaymentComponent';
+import AddTrainerComponent from './trainercomponent/AddTrainerComponent';
+import AddTrainingSchuduleComponent from './trainingschudulecomponent/AddTrainingSchuduleComponent';
 //import ReactLogo from './react.jpg';
 
 function App() {
@@ -21,15 +27,28 @@ function App() {
       
       <Header />
       
-      <Menu />
+      <UserMenu />
       <Router>
         <Routes>
         <Route exact path='/getStudent' element ={<GetStudentComponent />}/>
+        <Route exact path='/addstudent' element ={<AddStudentComponent />}/>
+
         <Route exact path='/getCourse' element ={<GetCourseComponent />}/>
+        <Route exact path='/addCourse' element ={<AddCourseComponent />}/>
+
         <Route exact path='/getPayment' element ={<GetPaymentComponent />}/>
+        <Route exact path='/addPayment' element ={<AddPaymentComponent />}/>
+
         <Route exact path='/getProgress' element ={<GetProgressComponent />}/>
+        <Route exact path='/addProgress' element ={<AddProgressComponent />}/>
+
         <Route exact path='/getTrainer' element ={<GetTrainerComponent />}/>
+        <Route exact path='/addTrainer' element ={<AddTrainerComponent />}/>
+
         <Route exact path='/getTrainingSchudule' element ={<TrainingSchuduleComponent />}/>
+        <Route exact path='/addTrainingSchudule' element ={<AddTrainingSchuduleComponent />}/>
+
+
         </Routes>
       </Router>
       <Footer />
