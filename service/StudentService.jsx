6e7edff65d3ae0aedc9189214e023cdac231c0a6
Alpha112355 <113292383+Alpha112355@users.noreply.github.com>
2022-09-13 10:44:25 +0530

    Update StudentService.jsx

--- service/StudentService.jsx
+++ service/StudentService.jsx
@@ -1,8 +1,12 @@
 import axios from 'axios'
-const STUDENT_REST_API_URL = ' http://localhost:8080/sms/Student';
+const STUDENT_REST_API_URL = ' http://localhost:8080/sms/student';
 class StudentService{
     getAllStudent(){
         return axios.get(STUDENT_REST_API_URL)
+    }
+
+    createStudent(student){
+        return axios.post(STUDENT_REST_API_URL , student)
     }
 }
 export default new StudentService();
