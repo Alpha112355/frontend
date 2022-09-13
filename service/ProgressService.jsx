0344003e8ec1d9fb124e7d03cf996ad1aa9420de
Alpha112355 <113292383+Alpha112355@users.noreply.github.com>
2022-09-13 10:43:58 +0530

    Update ProgressService.jsx

--- service/ProgressService.jsx
+++ service/ProgressService.jsx
@@ -6,5 +6,9 @@ class ProgressService{
     getAllProgress(){
         return axios.get(PROGRESS_REST_API_URL );
     }
+
+    createProgress(progress){
+        return axios.post(PROGRESS_REST_API_URL , progress)
+    }
 }
 export default new ProgressService();
