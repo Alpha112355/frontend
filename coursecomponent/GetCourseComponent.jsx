3a04723a78401b99e5a1bf48d8a856c2c916fe33
Alpha112355 <113292383+Alpha112355@users.noreply.github.com>
2022-09-14 07:28:43 +0530

    Update GetCourseComponent.jsx

--- coursecomponent/GetCourseComponent.jsx
+++ coursecomponent/GetCourseComponent.jsx
@@ -1,4 +1,5 @@
 import React , {useState,useEffect}from 'react'
+import {Link} from 'react-router-dom'
 import CourseService from '../service/CourseService'
 
 const GetCourseComponent = () => {
@@ -13,14 +14,25 @@ const GetCourseComponent = () => {
       
     }, [])
     
+    const deleteCourse=(courseId) => {
+        CourseService.deleteCourse(courseId).then((Response) => {
+
+        }).catch(error =>{
+            console.log(error);
+        })
+    }
 
   return (
     <div className='container'>
+    <br /><br />
+    <Link to = "/addCourse" className="btn btn-primary">Add Course</Link>
+    <br/><br/>
     <h2 className='text-center'>Course</h2>
         <table className='table table-bordered table-striped'>
             <thead>
                 <th>Course Id</th>
                 <th>Course</th>
+                <th>Action</th>
                
                 
             </thead>
@@ -31,7 +43,7 @@ const GetCourseComponent = () => {
                         <tr key={course.id}>
                             <td>{course.id}</td>
                             <td>{course.course}</td>
-                            
+                            <td><button className='btn btn-danger' onClick={( ) => deleteCourse(course.id)}>delete</button></td>
                         </tr>
                     )
                 }
