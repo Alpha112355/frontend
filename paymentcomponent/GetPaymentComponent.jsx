d1effc3810b63d0ab5ca27b3239201fc31fd4504
Alpha112355 <113292383+Alpha112355@users.noreply.github.com>
2022-09-14 07:29:15 +0530

    Update GetPaymentComponent.jsx

--- paymentcomponent/GetPaymentComponent.jsx
+++ paymentcomponent/GetPaymentComponent.jsx
@@ -1,4 +1,5 @@
 import React,{useState,useEffect} from 'react'
+import {Link} from 'react-router-dom'
 import PaymentService from '../service/PaymentService'
 
 
@@ -14,9 +15,20 @@ const GetPaymentComponent = () => {
         })
      
     }, [])
+
+    const deletePayment=(paymentId) => {
+        PaymentService.deletePayment(paymentId).then((Response) => {
+
+        }).catch(error =>{
+            console.log(error);
+        })
+    }
     
   return (
     <div className='container'>
+    <br /><br />
+    <Link to = "/addPayment" className="btn btn-primary">Add Payment</Link>
+    <br/><br/>
     <h2 className='text-center'>Payment</h2>
         <table className='table table-bordered table-striped'>
             <thead>
@@ -35,8 +47,8 @@ const GetPaymentComponent = () => {
                             <td>{payment.id}</td>
                             <td>{payment.decription}</td>
                             <td>{payment.paymentDate}</td>
-                            <td>{payment.amountpaid}</td>
-                            
+                            <td>{payment.amountPaid}</td>
+                            <td><button className='btn btn-danger' onClick={( ) => deletePayment(payment.id)}>delete</button></td>
                         </tr>
                     )
                 }
