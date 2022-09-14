ea1bc2f59e8749f093592328849132d42c3d16e8
Alpha112355 <113292383+Alpha112355@users.noreply.github.com>
2022-09-14 12:52:31 +0530

    Update Menu.jsx

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-const Menu = () => {
+const UserMenu = () => {
   return (
     <div>
         <table class="menu">
             <tr>
-                <td><a href="/" className="btn btn-primary">Home</a></td>
+              
                 <td><a href="/getStudent" className="btn btn-primary">Students</a></td>
-                <td><a href="/getProgress" className="btn btn-primary">Progress</a></td>
+                {/* <td><a href="/getProgress" className="btn btn-primary">Progress</a></td>
                 <td><a href="/getPayment" className="btn btn-primary">Payment</a></td>
                 <td><a href="/getCourse" className="btn btn-primary">Course</a></td>
                 <td><a href="/getTrainer" className="btn btn-primary">Trainer</a></td>
-                <td><a href="/getTrainingSchudule" className="btn btn-primary">TrainingSchudule</a></td>
+                <td><a href="/getTrainingSchudule" className="btn btn-primary">TrainingSchudule</a></td> */}
             </tr>
         </table>
     </div>
@@ -19,4 +19,4 @@ const Menu = () => {
   )
 }
 
-export default Menu
+export default UserMenu
